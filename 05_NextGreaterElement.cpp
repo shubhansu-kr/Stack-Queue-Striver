7bf1aa8f56c955a3303477445c4ec7203546d865
Shubhansu kumar <shubhansu2021@gmail.com>
2022-12-18 13:03:24 +0530

    FInd the next greater element in a cyclic array
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 05_NextGreaterElement.cpp
+++ 05_NextGreaterElement.cpp
@@ -3,18 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // For cyclic nums: 
+public:
+    vector<int> nextGreater(vector<int> &nums){
+        stack<int> stk;
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        for (int i = 2 * (nums.size()) -1; i >= 0; --i)
+        {
+            int ind = i % nums.size();
+            while(!stk.empty() && stk.top() <= nums[ind]){stk.pop();}
+            if (!stk.empty()){ans[ind] = stk.top();}
+            stk.push(nums[ind]);
+        }
+        return ans;
+    }
+};
+
 class Solution
 {
     // For non cyclic nums: 
 public:
     vector<int> nextGreater(vector<int> &nums){
         stack<int> stk;
-        vector<int> ans;
+        int n = nums.size();
+        vector<int> ans(n, -1);
         for (int i = nums.size()-1; i >= 0; --i)
         {
             while(!stk.empty() && stk.top() <= nums[i]){stk.pop();}
-            if (stk.empty()){ans[i] = -1;}
-            else {ans[i] = stk.top();}
+            if (!stk.empty()){stk.top();}
             stk.push(nums[i]);
         }
         return ans;
